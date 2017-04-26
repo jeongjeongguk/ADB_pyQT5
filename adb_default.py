e9c74eed3164024084a39c10016f69ea1db28433
DESKTOP-TEMC4I3\Jeongkuk <wjdrnr6176@gmail.com>
2017-04-26 17:45:32 +0900

    추후 확장성을 위해 record에 있는내용을 default로 옮김. 관련내용들 어느정도 정리되면, default를 mobileAndroid.py로 변경필요

--- adb_default.py
+++ adb_default.py
@@ -1,6 +1,10 @@
-import ctypes,os,time,pprint
+import ctypes
+import datetime
+import os
 import subprocess as cmd
+import time
 from xml.dom.minidom import parse
+
 
 class default(object):
     company = "C:\\Users\\Jeongkuk\\PycharmProjects\\androidADB\\apks"
@@ -12,17 +16,25 @@ class default(object):
         self.startActivity = ""
         self.ConnectDevices = []
         #self.DisconnectDevices = []
+        self.today = ""
+        self.currentTime = ""
+        self.deviceData = ""
+        self.makedir()
 
     @classmethod
     def check_connect(cls):
-        List_misi = cmd.check_output("adb devices | findstr device", stderr=cmd.STDOUT, shell=True) \
-                                        .decode("utf-8") \
-                                        .replace("List of devices attached", "") \
-                                        .replace("\r\n", "") \
-                                        .replace("device", "")
-        cls.ConnectDevices = List_misi.split("	")
-        cls.ConnectDevices.remove("")
-        return len(cls.ConnectDevices)
+        try:
+            # \r\n 제거는 윈도우기준
+            List_misi = cmd.check_output("adb devices | findstr device", stderr=cmd.STDOUT, shell=True) \
+                                            .decode("utf-8") \
+                                            .replace("List of devices attached", "") \
+                                            .replace("\r\n", "") \
+                                            .replace("device", "")
+            cls.ConnectDevices = List_misi.split("	")
+            cls.ConnectDevices.remove("")
+            return len(cls.ConnectDevices)
+        except:
+            return 0
 
     @classmethod
     def install_apk(cls, filepath): # def install_apk(self, filepath, option): # option : r, b, ...
@@ -102,16 +114,27 @@ class default(object):
         CHK_ANYDevice = cls.check_connect() #check_any_connect_device
         print(CHK_ANYDevice)
         if CHK_ANYDevice == 0 :
-            ctypes.windll.user32.MessageBoxW(0, "USB연결 및 드라이버설치 \n\n또는 개발자모드활성화를 확인하세요.", "연결된 기기없음", 0)
+            #TODO : HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            test = ctypes.windll.user32.MessageBoxW(0, "USB연결 및 드라이버설치 \n\n또는 개발자모드활성화를 확인하세요.", "연결된 기기없음", 0)
+            print(test)
         else :
             try :
+                cls.run_info(filepath_old) #알송기준 : com.estsoft.alsong
                 #TODO : 구버전 명시하지 않았고, 출시버전으로 확인시에만 사용. 구버전 apk 사용-> 해당apk설치
                 cmd.check_output(
                     "adb shell am start -a android.intent.action.VIEW -d \"https://play.google.com/store/apps/details?id=" \
                     + cls.packageName + "\"", stderr=cmd.STDOUT, shell=True)
                 cls.touchByID("com.android.vending:id/buy_button") # 플레이스토어 설치버튼 터치
                 cls.touchByID("com.android.vending:id/continue_button") # 플레이스토어 팝업뷰>설치버튼 터치
-                cls.run_apk(filepath_old) #TODO : 출시버전의 apk 추출해서 packagename, startactivity 구하기
+                time.sleep(30)
+
+                CHK_start = \
+                cmd.check_output("adb shell pm list package -f " + cls.packageName, stderr=cmd.STDOUT, shell=True) \
+                                    .decode("utf-8").split(":")[1].split("=")[0]
+                cmd.check_output("adb pull " + CHK_start + " test.apk", stderr=cmd.STDOUT, shell=True)
+                cls.run_apk("test.apk")
+                # cls.run_apk(filepath_old) #TODO : 출시버전의 apk 추출해서 packagename, startactivity 구하기
+
                 ctypes.windll.user32.MessageBoxW(0, "구버전에서 설정할 내용들 설정하세요.", "설정안내", 0)
                 time.sleep(120)
                 cls.reinstall_apk(filepath_new)
@@ -149,6 +172,59 @@ class default(object):
     def controlDevice(self, command):
         os.system(command)
 
+    @classmethod
+    def makedir(cls):
+        cls.today = datetime.datetime.now().strftime("%y%m%d")
+        check_folder = cmd.call("dir " + cls.today, stderr=cmd.STDOUT, shell=True)
+        if check_folder != False:
+            os.system("mkdir " + cls.today)
+            print("make directory " + cls.today)  # TODO: Need to change method to "pop-up message"
+        else:
+            print("생성된 폴더내에 파일이 저장됩니다.")  # TODO: Need to change method to "pop-up message"
+
+    @classmethod
+    def check_time(cls):
+        cls.currentTime = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+        print(cls.currentTime)
+
+    @classmethod
+    def device_info(cls, select_device):
+        select_device = ""  # TODO : delete this line
+        os_ver = cmd.check_output("adb shell " + select_device + "getprop ro.build.version.release",
+                                  stderr=cmd.STDOUT, shell=True).decode("utf-8").replace("\r\n", "")
+        api_level = cmd.check_output("adb shell " + select_device + "getprop ro.build.version.sdk",
+                                     stderr=cmd.STDOUT, shell=True).decode("utf-8").replace("\r\n", "")
+        model = cmd.check_output("adb shell " + select_device + "getprop ro.product.model",
+                                 stderr=cmd.STDOUT, shell=True).decode("utf-8").replace("\r\n", "")
+        cls.deviceData = model + "_" + os_ver + "_API" + api_level
+        print(cls.deviceData)
+
+    @classmethod
+    def capture2image(cls):#TODO : 기기 잠금화면 상태유무 확인후, 잠금해제 메소드 추가 필요
+        cls.check_connect()
+        os.system("adb shell rm -r /mnt/sdcard/ScreenCapture")
+        os.system("adb shell mkdir /mnt/sdcard/ScreenCapture")
+
+        os.system("adb shell screencap /mnt/sdcard/ScreenCapture/test.png")
+        os.system("adb pull /mnt/sdcard/ScreenCapture/test.png ./test.png")
+        os.system("adb shell rm /mnt/sdcard/ScreenCapture/test.png")
+        os.system("adb shell rm /mnt/sdcard/ScreenCapture/test.png")
+        cls.check_time()
+        cls.device_info(None)
+        changedName = cls.currentTime + "_" + cls.deviceData+".jpg"
+        os.system("ren test.png "+ changedName)
+        os.system("move " + changedName + " " +cls.today)
+
+    @classmethod
+    def capture2viedo(cls):
+        cls.check_connect()
+        cls.device_info(None)
+        # TODO : 4.4 이상일때에만 영상녹화하고, 메시지다이얼로그의 '녹화끝','취소'리턴받으면 녹화중지시키고 영상뺴오기
+        cls.check_time()
+        changedName = cls.currentTime + "_" + cls.deviceData + ".mp4"
+        os.system("ren test.mp4 " + changedName)
+        os.system("move " + changedName + " " + cls.today)
+        pass
 
 if __name__ == "__main__":
     #'''
@@ -165,12 +241,11 @@ if __name__ == "__main__":
     # test.uninstall_apk(filepath)
     # test.check_connect()
     # test.update(None,None)
-    filepath_new = "alsong_4.0.7.3.apk"
-    filepath_old = "Alsong_v3.810_1cha.apk"
-    test.update(filepath_old,filepath_new)
-
+    # filepath_new = "alsong_4.0.7.3.apk"
+    # filepath_old = "Alsong_v3.810_1cha.apk"
+    # test.update(filepath_old,filepath_new)
     # time.sleep(30)
     # filepath = "alsong_4.0.7.3.apk"
     # test.run_info(filepath)
     # test.install_apk(filepath)
-
+    # test.capture2image()
