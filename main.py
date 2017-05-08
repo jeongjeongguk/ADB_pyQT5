547eef90b5d8b2e37cb283d509b9e3ae3d7f759b
DESKTOP-TEMC4I3\Jeongkuk <wjdrnr6176@gmail.com>
2017-05-08 13:48:19 +0900

    main파일에서, ui와 adb기능파일 상속받아서, 메인창 띄움. 메인창에서 기능연결필요.

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import main_ui
 from PyQt5 import QtWidgets
 
 
-class MainWindow(main_ui.Ui_MainWindow, adb_default.default):
+class MainWindow(QtWidgets.QMainWindow, main_ui.Ui_MainWindow, adb_default.default):
     def __init__(self):
         super(self.__class__, self).__init__()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = MainWindow()
-    MainWindow.show()
+    Main = QtWidgets.QMainWindow()
+    ui = MainWindow()
+    ui.setupUi(Main)
+    Main.show()
     sys.exit(app.exec_())
